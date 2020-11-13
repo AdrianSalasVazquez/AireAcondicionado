cb4c8268e56692e7dac63b5c2d1caa183901f51e
Adrian <adriansalasvazquez@gmail.com>
2020-11-13 14:04:11 +0100

    0497.03 añadido maxima y minima temperatura con su incremento

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -1,20 +1,45 @@
 public class AireAcondicionado {
-    
+
     private double temperatura;
-    
-    public AireAcondicionado () {
+    private double max;
+    private double min;
+    private double incremento;
+
+    public AireAcondicionado (double maximo, double minimo) {
         temperatura = 15.0;
+        max = maximo;
+        min = minimo;
+        incremento = 5.0;
     }
-    
+
     public double getTemperatura() {
         return temperatura;
     }
     
-    public void subirTemperatura() {
-        temperatura = temperatura + 5.0;
+    public void setIncremento(double incrementoTemperatura) {
+        if (incrementoTemperatura < 0){
+            System.out.println("Error: No es un valor adecuado.");  
+        }
+        else{
+            incremento = incrementoTemperatura;
+        }
     }
-    
+
+    public void subirTemperatura() {
+        if ((temperatura + incremento) > max){
+            System.out.println("Error: No se puede incrementar mas la temperatura.");  
+        }
+        else{
+            temperatura = temperatura + incremento;
+        }
+    }
+
     public void bajarTemperatura() {
-        temperatura = temperatura - 5.0;
+        if ((temperatura - incremento) < min){
+            System.out.println("Error: No se puede reducir mas la temperatura.");  
+        }
+        else{
+            temperatura = temperatura - incremento;
+        }
     }
 }
