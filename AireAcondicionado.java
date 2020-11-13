52e8d1b599d988129787df2352ba73d816a8d3fd
Adrian <adriansalasvazquez@gmail.com>
2020-11-13 13:42:27 +0100

    0497.02 Añadido subir y bajar temperatura

--- AireAcondicionado.java
+++ AireAcondicionado.java
@@ -1,3 +1,20 @@
 public class AireAcondicionado {
     
+    private double temperatura;
+    
+    public AireAcondicionado () {
+        temperatura = 15.0;
+    }
+    
+    public double getTemperatura() {
+        return temperatura;
+    }
+    
+    public void subirTemperatura() {
+        temperatura = temperatura + 5.0;
+    }
+    
+    public void bajarTemperatura() {
+        temperatura = temperatura - 5.0;
+    }
 }
